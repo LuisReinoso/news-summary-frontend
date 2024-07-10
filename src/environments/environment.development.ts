791e3770ba3a23df16120b40dc26487ab099bf51
LuisReinoso <LuisReinoso@users.noreply.github.com>
2024-07-10 12:15:18 +0000

    deploy: 7743c046e193a47ebddb70f97049693dd9dd3744

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  url: 'http://localhost:4200',
-  backUrl: 'http://127.0.0.1:8000'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'https://luisreinoso.github.io/news-summary',
-  backUrl: 'https://news-summary.fly.dev'
-};
